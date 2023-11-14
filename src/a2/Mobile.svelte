b70c323733c3ceefe1310a8cba39e65d4aa615a6
Weston <classicmclive@gmail.com>
2023-11-14 10:35:24 -0500

    Create Mobile.svelte

---
+++ src/a2/Mobile.svelte
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="lead-mobile">
+  <h1>TheWestonFiles</h1>
+  Documentation For the Least
+</div>
+<main>
+  <div class="head"><h1>Nynorn</h1></div>
+  <p>Nynorn is a project that aims to revive the <a href="https://en.wikipedia.org/wiki/Norn_language">Norn language</a>. <a href="https://en.wikipedia.org/wiki/Norn_language">Norn</a> was the language spoken in the islands of <a href="https://en.wikipedia.org/wiki/Shetland">Shetland</a> and <a href="https://en.wikipedia.org/wiki/Orkney">Orkney</a>.<a href="https://thelanguagecloset.com/2021/07/24/reviving-a-lost-viking-language-the-nynorn-project/">[1]</a> Nynorn as a project was started around 2006 and aims to recreate the <a href="https://en.wikipedia.org/wiki/Shetland">Shetland</a> dialect with few plans to revive the <a href="https://en.wikipedia.org/wiki/Orkney">Orkney</a> dialect.<a href="https://thelanguagecloset.com/2021/07/24/reviving-a-lost-viking-language-the-nynorn-project/">[1]  </a> Nynorn as a project is still in the very early stages and is very experimental.<a href="https://nornlanguage.x10.mx/index.php?nynorn">[2]</a></p>
+  <div class="box-mobile">
+    <div class="box-contents">
+      <p class="box-item box-lead">Nynorn</p>
+      <p class="box-item">Hjetmål</p>
+      <div class="box-dash"></div>
+      <p class="box-descriptor">Family: <em>Germanic</em></p>
+      <p class="box-descriptor">Predecessor: <em>Norn</em></p>
+      <p class="box-descriptor">Script: <em>Latin</em></p>
+    </div>
+  </div>
+  <div class="head"><h1>Phonetics</h1></div>
+  <p>The alphabet consists of 24 letters. <a href="https://nornlanguage.x10.mx/index.php?nynorn">[2]</a><br/>
+    Various letters combination sounds.
+    </p>
+    <ul>
+      <li>hj sounds similar to the English j. <a href="https://nornlanguage.x10.mx/index.php?nynorn">[2]</a></li>
+      <li>sj sounds similar to the English sh. <a href="https://nornlanguage.x10.mx/index.php?nynorn">[2]</a></li>
+      <li>tsj sounds similar to the English ch. <a href="https://nornlanguage.x10.mx/index.php?nynorn">[2]</a></li>
+    </ul>
+  <div class="head"><h1>Alphabet</h1></div>
+  <div class="line-mobile">
+    <div class="line-item-mobile">Aa</div><div class="line-item-mobile">Bb</div><div class="line-item-mobile">Dd</div><div class="line-item-mobile">Ðð</div><div class="line-item-mobile">Ee</div><div class="line-item-mobile">Ff</div><div class="line-item-mobile">Gg</div><div class="line-item-mobile">Hh</div><br/>
+    <div class="line-item-mobile">Ii</div><div class="line-item-mobile">Jj</div><div class="line-item-mobile">Kk</div><div class="line-item-mobile">Ll</div><div class="line-item-mobile">Mm</div><div class="line-item-mobile">Nn</div><div class="line-item-mobile">Oo</div><div class="line-item-mobile">Pp</div><br/>
+    <div class="line-item-mobile">Rr</div><div class="line-item-mobile">Ss</div><div class="line-item-mobile">Tt</div><div class="line-item-mobile">Uu</div><div class="line-item-mobile">Vv</div><div class="line-item-mobile">Yy</div><div class="line-item-mobile">Øø</div><div class="line-item-mobile">Åå</div>
+  </div>
+  The capital Ð is not a grammatically used letter and is only included in the alphabet for completeness. <a href="https://nornlanguage.x10.mx/index.php?nynorn">[2]</a>
+  <div class="head"><h2>Sources</h2></div>
+  <p>1.<a href="https://thelanguagecloset.com/2021/07/24/reviving-a-lost-viking-language-the-nynorn-project/">^</a> Zui (2021-07-24). <a href="https://thelanguagecloset.com/2021/07/24/reviving-a-lost-viking-language-the-nynorn-project/">"Reviving a "lost" Viking language — The Nynorn Project" <i class="fa fa-external-link" style="font-size:12px;"></i></a>. The Language Closet. Retrieved 2022-10-17.<br/>
+    2.<a href="https://nornlanguage.x10.mx/index.php?nynorn">^</a> <a href="https://nornlanguage.x10.mx/index.php?nynorn">"Intoduction into the Nynorn Project" <i class="fa fa-external-link" style="font-size:12px;"></i></a>. nornlanguage.x10.mx. Retrieved 2022-10-17.</p>
+</main>
