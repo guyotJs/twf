4bcf80b1a710a2a826326209e424a39f1b246b6c
Weston <classicmclive@gmail.com>
2023-11-14 10:33:25 -0500

    Create App.svelte

---
+++ src/a2/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Body from './Body.svelte';
+  import Mobile from './Mobile.svelte';
+  let mobile = false;
+  if(window.innerWidth < window.innerHeight){
+    mobile = true;
+  }
+</script>
+{#if !mobile}
+  <Body/>
+  {:else}
+  <Mobile/>
+{/if}
